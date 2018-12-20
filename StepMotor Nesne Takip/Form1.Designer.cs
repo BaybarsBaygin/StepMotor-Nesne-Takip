f4573592ee3833e6ca87a9ecd8eebc17ff795c82
BaybarsBaygin <BaybarsBaygin@.github.com>
2018-12-20 07:54:50 +0300

    StepMotor Nesne Takip

---
+++ StepMotor Nesne Takip/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace StepMotor_Nesne_Takip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.kameraAc = new System.Windows.Forms.Button();
+            this.kameraDur = new System.Windows.Forms.Button();
+            this.seriPortB = new System.Windows.Forms.Button();
+            this.seriPortD = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Basla = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripComboBox2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(930, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 23);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(278, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // kameraAc
+            // 
+            this.kameraAc.Location = new System.Drawing.Point(12, 59);
+            this.kameraAc.Name = "kameraAc";
+            this.kameraAc.Size = new System.Drawing.Size(95, 23);
+            this.kameraAc.TabIndex = 2;
+            this.kameraAc.Text = "Kamera Bağlan";
+            this.kameraAc.UseVisualStyleBackColor = true;
+            this.kameraAc.Click += new System.EventHandler(this.kameraAc_Click);
+            // 
+            // kameraDur
+            // 
+            this.kameraDur.Location = new System.Drawing.Point(113, 59);
+            this.kameraDur.Name = "kameraDur";
+            this.kameraDur.Size = new System.Drawing.Size(75, 23);
+            this.kameraDur.TabIndex = 3;
+            this.kameraDur.Text = "Kamera Dur";
+            this.kameraDur.UseVisualStyleBackColor = true;
+            this.kameraDur.Click += new System.EventHandler(this.kameraDur_Click);
+            // 
+            // seriPortB
+            // 
+            this.seriPortB.Location = new System.Drawing.Point(12, 124);
+            this.seriPortB.Name = "seriPortB";
+            this.seriPortB.Size = new System.Drawing.Size(95, 23);
+            this.seriPortB.TabIndex = 4;
+            this.seriPortB.Text = "Seri Port Bağlan";
+            this.seriPortB.UseVisualStyleBackColor = true;
+            this.seriPortB.Click += new System.EventHandler(this.seriPortB_Click);
+            // 
+            // seriPortD
+            // 
+            this.seriPortD.Location = new System.Drawing.Point(113, 124);
+            this.seriPortD.Name = "seriPortD";
+            this.seriPortD.Size = new System.Drawing.Size(75, 23);
+            this.seriPortD.TabIndex = 5;
+            this.seriPortD.Text = "Seri Port Dur";
+            this.seriPortD.UseVisualStyleBackColor = true;
+            this.seriPortD.Click += new System.EventHandler(this.seriPortD_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Basla
+            // 
+            this.Basla.Location = new System.Drawing.Point(12, 88);
+            this.Basla.Name = "Basla";
+            this.Basla.Size = new System.Drawing.Size(75, 23);
+            this.Basla.TabIndex = 6;
+            this.Basla.Text = "Başlat";
+            this.Basla.UseVisualStyleBackColor = true;
+            this.Basla.Click += new System.EventHandler(this.Basla_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(209, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label3";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 162);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 17);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Kırmızı";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 185);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Yeşil";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 208);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(48, 17);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Mavi";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 548);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Basla);
+            this.Controls.Add(this.seriPortD);
+            this.Controls.Add(this.seriPortB);
+            this.Controls.Add(this.kameraDur);
+            this.Controls.Add(this.kameraAc);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button kameraAc;
+        private System.Windows.Forms.Button kameraDur;
+        private System.Windows.Forms.Button seriPortB;
+        private System.Windows.Forms.Button seriPortD;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Basla;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ StepMotor Nesne Takip/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision;
+using AForge.Vision.Motion;
+
+namespace StepMotor_Nesne_Takip
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureDevice camera;
+        private FilterInfoCollection camfilt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int X;
+        int Y = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            for (int i = 0; i < System.IO.Ports.SerialPort.GetPortNames().Length; i++)
+            {
+                toolStripComboBox2.Items.Add(System.IO.Ports.SerialPort.GetPortNames()[i]);
+            }
+            camfilt = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo VideoCaptureDevice in camfilt)
+            {
+                toolStripComboBox1.Items.Add(VideoCaptureDevice.Name);
+                toolStripComboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void kameraAc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                camera = new VideoCaptureDevice(camfilt[toolStripComboBox1.SelectedIndex].MonikerString);
+                camera.NewFrame += new NewFrameEventHandler(camera_NewFrame);
+                camera.DesiredFrameRate = 30;          
+                camera.DesiredFrameSize = new Size(640, 480);  
+                camera.Start();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("HİÇ KAMERA BULUNAMADI", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void camera_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap image1 = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = image;
+
+            if(radioButton1.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                filter.CenterColor = new RGB(Color.FromArgb(185, 45, 30));
+                filter.Radius = 50;
+                filter.ApplyInPlace(image1);
+                scala(image1);
+            }
+            if(radioButton2.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                filter.CenterColor = new RGB(Color.FromArgb(45, 185, 30));
+                filter.Radius = 50;
+                filter.ApplyInPlace(image1);
+                scala(image1);
+            }
+            if(radioButton3.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                filter.CenterColor = new RGB(Color.FromArgb(47, 30, 185));
+                filter.Radius = 50;
+                filter.ApplyInPlace(image1);
+                scala(image1);
+            }
+
+        }
+
+        private void scala(Bitmap image)
+        {
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.MinWidth = 2;
+            blobCounter.MinHeight = 2;
+            blobCounter.FilterBlobs = true;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
+
+            Grayscale grayFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap grayImage = grayFilter.Apply(image);
+
+            blobCounter.ProcessImage(grayImage);
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();
+            foreach (Rectangle recs in rects)
+            {
+
+                if (rects.Length > 0)
+                {
+                    Rectangle objectRect = rects[0];
+                    Graphics g = pictureBox1.CreateGraphics();
+                    using (Pen pen = new Pen(Color.FromArgb(252, 3, 26), 2))
+                    {
+
+                        g.DrawRectangle(pen, objectRect);
+                    }
+
+                    X = objectRect.X + (objectRect.Width / 2); 
+                    Y = objectRect.Y + (objectRect.Height / 2);
+                    g.DrawString(X.ToString() + "X" + Y.ToString(), new Font("Arial", 12), Brushes.Red, new System.Drawing.Point(250, 1));
+                    g.Dispose();
+                   
+                }
+            }
+        }
+
+        private void seriPortB_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = toolStripComboBox2.Text;
+            label2.Text = "Arduino'ya Bağlandı (" + toolStripComboBox2.Text + ")";
+            serialPort1.Open();
+        }
+
+        private void seriPortD_Click(object sender, EventArgs e)
+        {
+            label2.Text = "COM Port Seç !";
+            serialPort1.Close();
+        }
+
+        private void Basla_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void seriPortB_MouseMove(object sender, MouseEventArgs e)
+        {
+            seriPortB.BackColor = Color.White;
+        }
+
+        private void seriPortB_MouseLeave(object sender, EventArgs e)
+        {
+            seriPortB.BackColor = Color.FromArgb(192, 64, 0);
+        }
+
+        private void kameraDur_Click(object sender, EventArgs e)
+        {
+            if (camera.IsRunning)
+            {
+                camera.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (X >= 512) X = 256;
+            else if (X >= 500) X = 250;
+            else if (X >= 490) X = 245;
+            else if (X >= 480) X = 240;
+            else if (X >= 470) X = 235;
+            else if (X >= 460) X = 230;
+            else if (X >= 450) X = 225;
+            else if (X >= 440) X = 220;
+            else if (X >= 430) X = 215;
+            else if (X >= 420) X = 210;
+            else if (X >= 410) X = 205;
+            else if (X >= 400) X = 200;
+            else if (X >= 390) X = 195;
+            else if (X >= 380) X = 190;
+            else if (X >= 370) X = 185;
+            else if (X >= 360) X = 180;
+            else if (X >= 350) X = 175;
+            else if (X >= 340) X = 170;
+            else if (X >= 330) X = 165;
+            else if (X >= 320) X = 160;
+            else if (X >= 310) X = 155;
+            else if (X >= 300) X = 150;
+            else if (X >= 290) X = 145;
+            else if (X >= 280) X = 140;
+            else if (X >= 270) X = 135;
+            else if (X >= 260) X = 130;
+            else if (X >= 250) X = 125;
+            else if (X >= 240) X = 120;
+            else if (X >= 230) X = 115;
+            else if (X >= 220) X = 110;
+            else if (X >= 210) X = 105;
+            else if (X >= 200) X = 100;
+            else if (X >= 190) X = 95;
+            else if (X >= 180) X = 90;
+            else if (X >= 170) X = 85;
+            else if (X >= 160) X = 80;
+            else if (X >= 150) X = 75;
+            else if (X >= 140) X = 70;
+            else if (X >= 130) X = 65;
+            else if (X >= 120) X = 60;
+            else if (X >= 110) X = 55;
+            else if (X >= 100) X = 50;
+            else if (X >= 90) X = 45;
+            else if (X >= 80) X = 40;
+            else if (X >= 70) X = 35;
+            else if (X >= 60) X = 30;
+            else if (X >= 50) X = 25;
+            else if (X >= 40) X = 20;
+            else if (X >= 30) X = 15;
+            else if (X >= 20) X = 10;
+            else if (X >= 10) X = 5;
+            else X = 0;
+
+            int o = Convert.ToInt32(X);
+            byte[] b = BitConverter.GetBytes(o);
+            serialPort1.Write(b, 0, 4);
+            label4.Text = Convert.ToString(X);
+        }
+
+        private void kirmiziToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void yesilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void maviToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
